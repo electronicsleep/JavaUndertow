ae1878702910c62c07379152037de74f24712889
Chris <electronicsleep@gmail.com>
2021-05-13 14:28:59 -0700

    update-endpoints-start

--- src/main/java/com/java_undertow/Application.java
+++ src/main/java/com/java_undertow/Application.java
@@ -13,25 +13,27 @@ public class Application {
 
 	public static void main(String[] args) {
 		Undertow.Builder builder = Undertow.builder();
-		builder.setIoThreads(2);
+		builder.setIoThreads(5);
 		builder.setWorkerThreads(10);
 		String host = "0.0.0.0";
 		int port = 8080;
 		builder.addHttpListener(port, host);
 		System.out.println("Server started: http://" + host + ":" + port);
 		builder.setHandler(path()
-				.addPrefixPath("/test", exchange -> {
-					logger.info("test endpoint");
+				.addPrefixPath("/", exchange -> {
+					logger.info("Root endpoint");
 					exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
-					exchange.getResponseSender().send("test endpoint");
+					exchange.getResponseSender().send("root endpoint");
 	            }).addPrefixPath("/api", exchange -> {
 					logger.info("API endpoint");
+					ScoreApi scoreApi = new ScoreApi();
+					scoreApi.test();
 					exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
-					exchange.getResponseSender().send("{\"status\": \"up\"}");
+					exchange.getResponseSender().send("{\"api\": \"Up\"}");
 				}).addPrefixPath("/health", exchange -> {
 					logger.info("Health endpoint");
-					exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
-					exchange.getResponseSender().send("{\"status\": \"up\"}");
+					exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+					exchange.getResponseSender().send("{\"status\": \"Up\"}");
 				})
 		);
 		Undertow server = builder.build();
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gradle clean build
+java -jar build/libs/JavaUndertow.jar
