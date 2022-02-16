383cbe0a21b899a8c289c89113e356bec0467356
Chris <electronicsleep@gmail.com>
2022-02-15 21:10:06 -0800

    adding-constants

---
+++ src/main/java/com/imgidea/java_undertow/Constants.java
@@ -0,0 +1,6 @@
+package com.imgidea.java_undertow;
+public final class Constants {
+    public static final String HTML_HEADER = "<!DOCTYPE html>\n<html lang=\"en\">\n<body>" +
+            "<a href=\"/\">Home</a> <a href=\"/status\">Status</a><br>\n";
+    public static final String HTML_FOOTER = "</body>\n</html>\n";
+}
